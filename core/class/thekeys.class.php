f7394d94b0edf4ac7e7ce4610fb864e8e6de385d
Cédric Dupont <lunarok@gmail.com>
2017-08-16 08:35:46 +0200

    update with gateway methods

--- core/class/thekeys.class.php
+++ core/class/thekeys.class.php
@@ -48,50 +48,78 @@ class thekeys extends eqLogic {
                 $thekeys->save();
             }
             $thekeys->loadCmdFromConf($thekeys->getConfiguration('type'));
-            $value = ($key['etat'] == 'open') ? 1:0;
+            $value = ($key['etat'] == 'open') ? 0:1;
             $thekeys->checkAndUpdateCmd('status',$value);
             $thekeys->checkAndUpdateCmd('battery',$key['battery']/1000);
             $thekeys->batteryStatus($key['battery']/40);
-            $url = 'partage/all/serrure/' . $key['id'];
-            $json = thekeys::callCloud($url);
         }
     }
 
     public function allowLockers() {
-      thekeys::authCloud();
-      $idgateway = $this->getConfiguration('idfield');
-      foreach (eqLogic::byType('thekeys', true) as $location) {
-        if ($location->getConfiguration('type') == 'locker') {
-          $url = 'https://api.the-keys.fr/fr/api/v2/partage/create/' . $location->getConfiguration('id') . '/accessoire/' . $idgateway;
-          $data = array('partage_accessoire[description]' => '', 'partage_accessoire[nom]' => $this->getName());
-          $request_http = new com_http($url);
-          $request_http->setHeader(array('Authorization: Bearer ' . config::byKey('token','thekeys')));
-          $request_http->setPost($data);
-          $output = $request_http->exec(30);
-          $result = json_decode($output, true);
-          log::add('thekeys', 'debug', 'Retour : ' . print_r($result, true));
+        thekeys::checkShare();
+        $this->scanLockers();
+        thekeys::authCloud();
+        $idgateway = $this->getConfiguration('idfield');
+        foreach (eqLogic::byType('thekeys', true) as $location) {
+            if ($location->getConfiguration('type') == 'locker' && $location->getConfiguration('share' . $idgateway, 0) != 1 && $location->getConfiguration('accessible' . $idgateway, 0) == 1) {
+                $url = 'partage/create/' . $location->getConfiguration('id') . '/accessoire/' . $idgateway;
+                $data = array('partage_accessoire[description]' => '', 'partage_accessoire[nom]' => $this->getName());
+                $json = thekeys::callCloud($url,$data);
+            }
         }
-      }
+    }
+
+    public function scanLockers() {
+        $idgateway = $this->getConfiguration('idfield');
+        $json = $this->callGateway('search');
+        foreach ($json['devices'] as $device) {
+            $thekeys = self::byLogicalId($device['identifier'], 'thekeys');
+            if (!is_object($thekeys)) {
+                return;
+            }
+            $thekeys->setConfiguration('rssi',$device['rssi']);
+            $thekeys->setConfiguration('visible' . $location->getConfiguration('id'),1);
+            $thekeys->save();
+            $value = ($key['etat'] == 'open') ? 0:1;
+            $thekeys->checkAndUpdateCmd('status',$value);
+            $thekeys->checkAndUpdateCmd('battery',$key['battery']/1000);
+            $thekeys->batteryStatus($key['battery']/40);
+        }
+    }
+
+    public function checkShare() {
+        thekeys::authCloud();
+        foreach (eqLogic::byType('thekeys', true) as $location) {
+            if ($location->getConfiguration('type') == 'locker') {
+                $url = 'partage/all/serrure/' . $this->getConfiguration('id');
+                $json = thekeys::callCloud($url);
+                //update 'share' . $idtrouve + infos sur la plage horaire
+                foreach ($json['data']['partages_accessoire'] as $share) {
+                    $this->setConfiguration('share' . $share['accessoire']['id_accessoire'],1);
+                    $this->setConfiguration('code' . $share['accessoire']['id_accessoire'],$share['accessoire']['code']);
+                }
+            }
+        }
     }
 
     public function postAjax() {
-      if ($this->getConfiguration('typeSelect') != $this->getConfiguration('type')) {
-        $this->setConfiguration('type',$this->getConfiguration('typeSelect'));
-        $this->save();
-      }
-      $this->loadCmdFromConf($this->getConfiguration('type'));
-      if ($this->getConfiguration('type') == 'gateway') {
-        $this->allowLockers();
-      }
+        if ($this->getConfiguration('typeSelect') != $this->getConfiguration('type')) {
+            $this->setConfiguration('type',$this->getConfiguration('typeSelect'));
+            $this->save();
+        }
+        $this->loadCmdFromConf($this->getConfiguration('type'));
+        if ($this->getConfiguration('type') == 'gateway') {
+            $this->allowLockers();
+        }
     }
 
     public function loadCmdFromConf($type) {
         if (!is_file(dirname(__FILE__) . '/../config/devices/' . $type . '.json')) {
-          return;
+            return;
         }
         $content = file_get_contents(dirname(__FILE__) . '/../config/devices/' . $type . '.json');
         if (!is_json($content)) {
-          return;
+            return;
         }
         $device = json_decode($content, true);
         if (!is_array($device) || !isset($device['commands'])) {
@@ -103,35 +131,57 @@ class thekeys extends eqLogic {
         $this->import($device);
     }
 
-    public function cron5() {
+    public function cron15() {
+        foreach (eqLogic::byType('thekeys', true) as $location) {
+            if ($location->getConfiguration('type') == 'gateway') {
+                $location->scanLockers();
+            }
+        }
+    }
+
+    public function cronHourly() {
         thekeys::updateUser();
+        thekeys::checkShare();
     }
 
     public function pageConf() {
         thekeys::updateUser();
+        thekeys::checkShare();
     }
 
-    public function callCloud($url,$param = "json") {
+    public function callCloud($url,$data = array('format' => 'json')) {
         $url = 'https://api.the-keys.fr/fr/api/v2/' . $url;
-        if ($param == "json") {
-          $url .= '?_format=json';
+        if (isset($data['format'])) {
+            $url .= '?_format=' . $data['format'];
         }
         if (time() > config::byKey('timestamp','thekeys')) {
             thekeys::authCloud();
         }
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL,$url);
-        $headers = [
-            'Authorization: Bearer ' . config::byKey('token','thekeys')
-        ];
-        if ($param != "json") {
-          $data = array('partage_accessoire[description]' => '', 'partage_accessoire[nom]' => $param);
-
+        $request_http = new com_http($url);
+        $request_http->setHeader(array('Authorization: Bearer ' . config::byKey('token','thekeys')));
+        if (!isset($data['format'])) {
+            $request_http->setPost($data);
         }
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($curl,CURLOPT_RETURNTRANSFER , 1);
-        $json = json_decode(curl_exec($curl), true);
-        curl_close ($curl);
+        $output = $request_http->exec(30);
+        $json = json_decode($output, true);
+        log::add('thekeys', 'debug', 'URL : ' . $url);
+        //log::add('thekeys', 'debug', 'Authorization: Bearer ' . config::byKey('token','thekeys'));
+        log::add('thekeys', 'debug', 'Retour : ' . print_r($json, true));
+        return $json;
+    }
+
+    public function callGateway($uri,$id = '', $code = '') {
+        $url = 'http://' . $this->getConfiguration('ipfield') . '/' . $uri;
+        $request_http = new com_http($url);
+        if ($uri != ' search') {
+            $ts = time();
+            $key = hash_hmac('sha512',$ts,$code);
+            $has = base64_encode($key);
+            $data = array('hash' => $hash, 'identifier' => $id, 'ts' => $ts);
+            $request_http->setPost($data);
+        }
+        $output = $request_http->exec(30);
+        $json = json_decode($output, true);
         log::add('thekeys', 'debug', 'URL : ' . $url);
         //log::add('thekeys', 'debug', 'Authorization: Bearer ' . config::byKey('token','thekeys'));
         log::add('thekeys', 'debug', 'Retour : ' . print_r($json, true));
@@ -142,25 +192,15 @@ class thekeys extends eqLogic {
         $url = 'https://api.the-keys.fr/api/login_check';
         $user = config::byKey('username','thekeys');
         $pass = config::byKey('password','thekeys');
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL,$url);
-        curl_setopt($curl, CURLOPT_POST, 1);
-        $headers = [
-            'Content-Type: application/x-www-form-urlencoded'
-        ];
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-        $fields = array(
-        	'_username' => urlencode($user),
-        	'_password' => urlencode($pass),
+        $request_http = new com_http($url);
+        $request_http->setHeader(array('Content-Type: application/x-www-form-urlencoded'));
+        $data = array(
+            '_username' => $user,
+            '_password' => $pass,
         );
-        $fields_string = '';
-        foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-        rtrim($fields_string, '&');
-        curl_setopt($curl,CURLOPT_POST, count($fields));
-        curl_setopt($curl,CURLOPT_POSTFIELDS, $fields_string);
-        curl_setopt($curl,CURLOPT_RETURNTRANSFER , 1);
-        $json = json_decode(curl_exec($curl), true);
-        curl_close ($curl);
+        $request_http->setPost($data);
+        $output = $request_http->exec(30);
+        $json = json_decode($output, true);
         $timestamp = time() + (2 * 60 * 60);
         config::save('token', $json['token'],  'thekeys');
         config::save('timestamp', $timestamp,  'thekeys');
